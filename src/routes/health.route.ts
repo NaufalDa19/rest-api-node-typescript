70ea36719e231f0579af28ec2d54e85a69d1626f
NaufalDa19 <dwialrizqi8@gmail.com>
2023-02-13 22:39:50 +0700

    Check auto deploy

--- src/routes/health.route.ts
+++ src/routes/health.route.ts
@@ -6,5 +6,5 @@ export const HealthRouter: Router = Router()
 // http://localhost:3000/health
 HealthRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
   logger.info('Healthy check success')
-  res.status(200).send({ status: '200' })
+  res.status(200).send({ status: '200', message: 'Server is running' })
 })
